315f7288801e35c981a4bde5e71ea0b6aa2efcba
Matiullah Karimi <matiullah.karimi@gmail.com>
2016-08-27 16:23:58 +0430

    change dashboard to dashboard attendance.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Attendance Dashboard</div>
 
                 <div class="panel-body">
                     <a href="{{url('teachers')}}" class="btn btn-info">Teacher Entry</a>
